14a4d5d12dd060d03f1b8168baaa22fe5e58a464
LWM <minow@ezinc.co.kr>
2024-02-23 17:45:18 +0900

    슬라이드바 관련 작업 중

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import { BrowserRouter as Router, Routes, Route, Link, Navigate } from 'react-router-dom';
 import Card from './components/Card';
+import Sidebar from './components/Sidebar'; // 사이드바 컴포넌트 import
 import TextDiffViewer from './pages/TextDiffViewer';
 import DarkModeToggle from './components/DarkModeToggle';
 import Modal from './components/Modal';
@@ -9,51 +10,72 @@ import logo from './assets/imgs/logo.png';
 
 const App = () => {
   const [showCards, setShowCards] = useState(true);
+  const [sidebarOpen, setSidebarOpen] = useState(false);
   const [showModal, setShowModal] = useState(false);
-  const [navigateHome, setNavigateHome] = useState(false); // 홈으로 리다이렉트하는 상태 추가
-  const [logoClass, setLogoClass] = useState(''); // 로고의 클래스 상태를 관리합니다
-  const [textClass, setTextClass] = useState(''); // 텍스트의 클래스 상태를 관리합니다
+  const [navigateHome, setNavigateHome] = useState(false);
+  const [logoClass, setLogoClass] = useState('');
+  const [textClass, setTextClass] = useState('');
+  const [isAnimating, setIsAnimating] = useState(false);
+  const [logoContainerStyle, setLogoContainerStyle] = useState({});
 
   const handleCardClick = () => {
+    if (isAnimating) return;
+    setIsAnimating(true);
     setShowCards(false);
-    setNavigateHome(false); // 카드 클릭 시에는 홈으로 리다이렉트하지 않음
-    setLogoClass('logo-slide-out'); // 로고에 애니메이션 클래스를 적용합니다
+    setNavigateHome(false);
+    setLogoClass('logo-slide-out');
     setTextClass('text-fade-out');
+    // 로고의 가로 길이만큼 영역을 부여합니다.
+    setLogoContainerStyle({flexGrow: 0, flexShrink: 0, flexBasis: '100px'});
+    setSidebarOpen(true);
+
+    setTimeout(() => {
+      setIsAnimating(false);
+    }, 1000);
   };
 
   const handleNotOpenCardClick = () => {
     setShowModal(true);
   };
 
-  const handleCloseModal  = () => {
+  const handleCloseModal = () => {
     setShowModal(false);
   };
 
   const handleLogoClick = () => {
+    if (isAnimating || showCards) return;
+    setIsAnimating(true);
     setShowCards(true);
     setNavigateHome(true);
-    setLogoClass('logo-slide-in'); // 로고에 slide-in 애니메이션 클래스를 적용합니다
-    setTextClass('text-fade-in'); // 텍스트에 fade-in 애니메이션 클래스를 적용합니다
+    setLogoClass('logo-slide-in');
+    setTextClass('text-fade-in');
+    setLogoContainerStyle({});
+    setSidebarOpen(false);
+
+    setTimeout(() => {
+      setIsAnimating(false);
+    }, 1000);
   };
 
   return (
     <Router>
       <div className="App" style={{padding: '20px'}}>
-        <header className="App-header">
+        <header className="App-header" style={logoContainerStyle}>
           <div className={`center-container ${logoClass}`} onClick={handleLogoClick}>
-            <img src={logo} alt="ByteFactory" style={{cursor: 'pointer'}}/>
+            <img src={logo} alt="ByteFactory" style={{cursor: 'pointer'}} />
             <p className={`${textClass}`} style={{fontWeight: 'bold', cursor: 'pointer'}}>B Y T E F A C T O R Y</p>
           </div>
           <div className="right-container">
             <DarkModeToggle />
           </div>
         </header>
+        <Sidebar isOpen={sidebarOpen} />
         {showCards && (
           <div>
             <Link to="/TextDiffViewer" onClick={handleCardClick}>
               <Card title="TextDiffViewer" description="텍스트 비교 프로그램입니다."/>
             </Link>
-            <Link to="/b" onClick={handleNotOpenCardClick}>
+            <Link to="#" onClick={handleNotOpenCardClick}>
               <Card title="서비스 2" description="준비중입니다." />
             </Link>
             <Modal show={showModal} onClose={handleCloseModal}>
@@ -65,7 +87,6 @@ const App = () => {
         {!navigateHome && (
           <Routes>
             <Route path="/TextDiffViewer" element={<TextDiffViewer />} />
-            {/* <Route path="/b" element={<B />} /> 여기서 B 컴포넌트를 정의하고 element로 추가해야 합니다. */}
           </Routes>
         )}
       </div>
