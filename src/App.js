3576ed358232753e3ea80a980773b25ce946059f
LWM <minow@ezinc.co.kr>
2024-02-23 15:35:34 +0900

    카드 클릭 시 로고 좌측으로 슬라이딩 배치

--- src/App.js
+++ src/App.js
@@ -9,23 +9,26 @@ import logo from './assets/imgs/logo.png';
 const App = () => {
   const [showCards, setShowCards] = useState(true);
   const [navigateHome, setNavigateHome] = useState(false); // 홈으로 리다이렉트하는 상태 추가
+  const [logoClass, setLogoClass] = useState(''); // 로고의 클래스 상태를 관리합니다
 
   const handleCardClick = () => {
     setShowCards(false);
     setNavigateHome(false); // 카드 클릭 시에는 홈으로 리다이렉트하지 않음
+    setLogoClass('logo-slide-out'); // 로고에 애니메이션 클래스를 적용합니다
   };
 
   const handleLogoClick = () => {
     setShowCards(true); // 모든 카드를 다시 표시
     setNavigateHome(true); // 홈으로 리다이렉트
+    setLogoClass(''); // 로고 애니메이션 클래스를 초기화합니다
   };
 
   return (
     <Router>
       <div className="App" style={{padding: '20px'}}>
         <header className="App-header">
-          <div className="center-container" onClick={handleLogoClick}>
-            <img src={logo} alt="ByteFactory" style={{cursor: 'pointer'}}/>B Y T E F A C T O R Y
+          <div className={`center-container ${logoClass}`} onClick={handleLogoClick}>
+            <img src={logo} alt="ByteFactory" style={{cursor: 'pointer'}}/><p style={{fontWeight: 'bold', cursor: 'pointer'}}>B Y T E F A C T O R Y</p>
           </div>
           <div className="right-container">
             <DarkModeToggle />
