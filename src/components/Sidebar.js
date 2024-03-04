d1b9f627b23ea199ae7000de617b65aa416ce955
WonmoLee <leewonmo94@gmail.com>
2024-03-04 11:58:28 +0900

    사이드바 아이템 클릭시 헤더 명칭 변경

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -2,20 +2,21 @@ import React from 'react';
 import { useNavigate } from 'react-router-dom';
 import '../assets/Sidebar.css'; // 사이드바 CSS 스타일을 여기에 정의
 
-const Sidebar = ({ isOpen }) => {
+const Sidebar = ({ isOpen, setHeaderTitle }) => {
   const navigate = useNavigate();
 
-  const handleItemClick = (path) => {
+  const handleItemClick = (path, title) => {
     navigate(path);
+    setHeaderTitle(title);
   };
 
   return (
     <div className={`sidebar ${isOpen ? 'open' : ''}`}>
-      <div className="sidebar-item" onClick={() => handleItemClick('/TextDiffViewer')}>
+      <div className="sidebar-item" onClick={() => handleItemClick('/TextDiffViewer', 'TextDiffViewer')}>
         <i className="icon-home"></i> {/* 이곳에 홈 아이콘 SVG 또는 Font Awesome 아이콘을 사용할 수 있습니다 */}
         <span>텍스트 비교</span>
       </div>
-      <div className="sidebar-item" onClick={() => handleItemClick('/ClipboardMonitor')}>
+      <div className="sidebar-item" onClick={() => handleItemClick('/ClipboardMonitor', 'ClipboardMonitor')}>
         <i className="icon-messages"></i> {/* 이곳에 메시지 아이콘 SVG 또는 Font Awesome 아이콘을 사용할 수 있습니다 */}
         <span>간편 복사</span>
       </div>
