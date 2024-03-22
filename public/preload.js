6b2b8ae7e350fb95642f50f420e8d2e56f3a5f3f
WonmoLee <leewonmo94@gmail.com>
2024-03-22 15:21:48 +0900

    모달 페이지 추가 및 로직 컨텍스트브릿지 적용

--- public/preload.js
+++ public/preload.js
@@ -7,22 +7,9 @@ contextBridge.exposeInMainWorld('electron', {
   },
   send: (channel, data) => {
     // 화이트리스트에 있는 채널에서만 메시지를 보내도록 합니다.
-    let validChannels = ["open-link-external"];
+    let validChannels = ["open-link-external", "update-confirm-response"];
     if (validChannels.includes(channel)) {
       ipcRenderer.send(channel, data);
     }
   }
-});
-
-window.addEventListener('DOMContentLoaded', () => {
-  const acceptButton = document.getElementById('accept');
-  const laterButton = document.getElementById('later');
-
-  acceptButton.addEventListener('click', () => {
-    ipcRenderer.send('update-confirm-response', true);
-  });
-
-  laterButton.addEventListener('click', () => {
-    ipcRenderer.send('update-confirm-response', false);
-  });
 });
